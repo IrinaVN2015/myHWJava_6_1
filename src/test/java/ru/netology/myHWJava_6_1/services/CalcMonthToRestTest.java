2c1311d0a129a1acfa61a1b87206a2454d8f17bf
Irina Vilkova <vilkovairina.80@mail.ru>
2023-04-19 11:16:36 +0300

    Change CalcMonthToRestTest

--- src/test/java/ru/netology/myHWJava_6_1/services/CalcMonthToRestTest.java
+++ src/test/java/ru/netology/myHWJava_6_1/services/CalcMonthToRestTest.java
@@ -2,20 +2,24 @@ package ru.netology.myHWJava_6_1.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 //import ru.netology.myHWJava_6_1.services.CalcMonthToRest;
 
 public class CalcMonthToRestTest {
 
-    @Test
-    public void shouldCalcMonthToRestVar1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/rest.csv")
+    public void shouldCalcMonthToRestVar1(int expected, int income, int expenses, int threshold) {
         CalcMonthToRest service = new CalcMonthToRest();
 
-        int expected = 3;
-        int actual = service.calculate(10000, 3000, 20000);
+        // int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
+ /*   @Test
     public void shouldCalcMonthToRestVar2() {
         CalcMonthToRest service = new CalcMonthToRest();
 
@@ -23,5 +27,6 @@ public class CalcMonthToRestTest {
         int actual = service.calculate(100_000, 60_000, 150_000);
         Assertions.assertEquals(expected, actual);
     }
+  */
 }
 
